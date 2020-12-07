a728588831bb3d58012fb19af1f6dccc2be0c406
pengjiang <1114307274@qq.com>
2020-12-07 10:27:34 +0800

    1.头文件添加

---
+++ esayServer/include/cell_timestamp.hpp
@@ -0,0 +1,48 @@
+#ifndef _CELL_TIMESTAMP_
+#define _CELL_TIMESTAMP_
+#include <chrono>
+using namespace std::chrono;
+class cellTimeStamp
+{
+private:
+    time_point<high_resolution_clock> m_begin;
+
+public:
+    cellTimeStamp();
+    virtual ~cellTimeStamp();
+    void update();
+    double getElapsedSecond();
+    double getElapsedSecondInMilliSec();
+    int64_t getElapsedSecondInMicroSec();
+};
+
+cellTimeStamp::cellTimeStamp()
+{
+    m_begin = high_resolution_clock::now();
+}
+
+cellTimeStamp::~cellTimeStamp()
+{
+}
+
+void cellTimeStamp::update()
+{
+    m_begin = high_resolution_clock::now();
+}
+
+double cellTimeStamp::getElapsedSecond()
+{
+    return getElapsedSecondInMilliSec() * 0.001;
+}
+
+double cellTimeStamp::getElapsedSecondInMilliSec()
+{
+    return getElapsedSecondInMicroSec() * 0.001;
+}
+
+int64_t cellTimeStamp::getElapsedSecondInMicroSec()
+{
+    return duration_cast<microseconds>(high_resolution_clock::now() - m_begin).count();
+}
+
+#endif
